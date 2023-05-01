574b39ad9a083e1e41c18541c55850aa83f59a7e
Fasika Dagnachew <fasika1515@gmail.com>
2023-05-02 00:25:01 +0300

    update hero

---
+++ pages/Hero.jsx
@@ -0,0 +1,160 @@
+import Image from "next/image";
+import style from "../styles/Home.module.css"
+import { useRef, useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion"
+import { FaChevronCircleRight, FaChevronCircleLeft } from "react-icons/fa"
+
+const variant1 = {
+    hidden: {
+        opacity: 0,
+        y: 20
+    },
+    visible: {
+        opacity: [0, 0, 1],
+        y: [20, 20, 0],
+        transition: {
+            delay: .5,
+            duration: 1,
+            ease: "easeInOut"
+        }
+    },
+    exit: {
+        opacity: 0,
+        y: 20,
+        transition: {
+            duration: .5,
+            ease: "easeInOut"
+        }
+    }
+}
+const variant2 = {
+    hidden: {
+        opacity: 0,
+        y: -20
+    },
+    visible: {
+        opacity: [0, 0, 1],
+        y: [-20, -20, 0],
+        transition: {
+            delay: .7,
+            duration: 1,
+            ease: "easeInOut"
+        }
+    },
+    exit: {
+        opacity: 0,
+        y: -20,
+        transition: {
+            duration: .5,
+            ease: "easeInOut"
+        }
+    }
+}
+
+
+export default function Hero() {
+    const [current, setCurrent] = useState(0)
+    const slideRef = useRef(null)
+    const length = slideRef.current ? slideRef.current.children.length : 0
+
+    const nextSlide = () => {
+        if (slideRef.current) {
+            if (current === length - 1) {
+                setCurrent(0)
+            } else {
+                setCurrent(current + 1)
+            }
+        }
+    }
+
+    const prevSlide = () => {
+        if (slideRef.current) {
+            if (current === 0) {
+                setCurrent(length - 1)
+            } else {
+                setCurrent(current - 1)
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (slideRef.current) {
+            slideRef.current.style.transform = `translateY(-${current * 100}%)`
+        }
+    }, [current])
+
+  return (
+    <div className={style.hero}>
+        <div className={style.hero__slider__container}>
+
+            <div className={`${style.hero__slider__btn} ${style.prev}`}>
+                <button onClick={prevSlide}> <FaChevronCircleLeft /> </button>
+            </div>
+            <div className={`${style.hero__slider__btn} ${style.next}`}>
+                <button onClick={nextSlide}> <FaChevronCircleRight /> </button>
+            </div>
+
+            <div ref={slideRef} className={style.hero__slider}>
+                <div className={style.hero__slide}>
+                    <Image src="/images/home/simien.jpg" alt="hero1" width={1920} height={1080} />
+                    <motion.div className={style.hero__slide__content}>
+                        <AnimatePresence>
+                            {current === 0 && (
+                                <>
+                                    <motion.h1 variants={variant1} initial="hidden" animate="visible" exit="exit" >THE NORTHERN CIRCUIT</motion.h1>
+                                    <motion.p variants={variant2} initial="hidden" animate="visible" exit="exit" >An experience that is truly unique and awe-inspiring.</motion.p>
+                                    <motion.button variants={variant2} initial="hidden" animate="visible" exit="exit" >More Detail</motion.button>
+                                </>
+                            )}
+                        </AnimatePresence>
+                    </motion.div>
+                </div>
+                <div className={style.hero__slide}>
+                    <Image src="/images/home/omo.jpg" alt="hero1" width={1920} height={1080} />
+                    <motion.div className={style.hero__slide__content}>
+                        <AnimatePresence>
+                            {current === 1 && (
+                                <>
+                                    <motion.h1 variants={variant1} initial="hidden" animate="visible" exit="exit" >THE SOUTHERN CIRCUIT</motion.h1>
+                                    <motion.p variants={variant2} initial="hidden" animate="visible" exit="exit" >An experience that is truly unique and awe-inspiring.</motion.p>
+                                    <motion.button variants={variant2} initial="hidden" animate="visible" exit="exit" >More Detail</motion.button>
+                                </>
+                            )}
+                        </AnimatePresence>
+                    </motion.div>
+                </div>
+                <div className={style.hero__slide}>
+                    <Image src="/images/home/harar.jpg" alt="hero1" width={1920} height={1080} />
+                    <motion.div className={style.hero__slide__content}>
+                        <AnimatePresence>
+                            {current === 2 && (
+                                <>
+                                    <motion.h1 variants={variant1} initial="hidden" animate="visible" exit="exit" >THE EASTERN CIRCUIT</motion.h1>
+                                    <motion.p variants={variant2} initial="hidden" animate="visible" exit="exit" >An experience that is truly unique and awe-inspiring.</motion.p>
+                                    <motion.button variants={variant2} initial="hidden" animate="visible" exit="exit" >More Detail</motion.button>
+                                </>
+                            )}
+                        </AnimatePresence>
+                    </motion.div>
+                </div>
+                <div className={style.hero__slide}>
+                    <Image src="/images/home/west.jpg" alt="hero1" width={1920} height={1080} />
+                    <motion.div className={style.hero__slide__content}>
+                        <AnimatePresence>
+                            {current === 3 && (
+                                <>
+                                    <motion.h1 variants={variant1} initial="hidden" animate="visible" exit="exit" >THE WESTERN CIRCUIT</motion.h1>
+                                    <motion.p variants={variant2} initial="hidden" animate="visible" exit="exit" >An experience that is truly unique and awe-inspiring.</motion.p>
+                                    <motion.button variants={variant2} initial="hidden" animate="visible" exit="exit" >More Detail</motion.button>
+                                </>
+                            )}
+                        </AnimatePresence>
+                    </motion.div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
---
+++ pages/Navbar.jsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect, useRef } from 'react'
+import { motion } from 'framer-motion'
+import { AiOutlineClose } from "react-icons/ai"
+import { HiOutlineMenu } from "react-icons/hi"
+
+export default function Navbar() {
+  const mobileRef = useRef(null)
+    const [mobile, setMobile] = useState(false)
+    
+    const toggleMobile = () => {
+        
+        setMobile(!mobile)
+        // console.log(mobile)
+    }
+
+    useEffect(() => {
+
+        if (mobile) {
+            mobileRef.current.style.transform = 'translateX(0%)'
+        } else {
+            mobileRef.current.style.transform = 'translateX(100%)'
+        }
+
+    }, [mobile])
+
+
+  return (
+    <>
+      <div className="nav">
+          <div className="nav__logo">
+              <Image src="/images/logo/logo.png" alt="BlueNileExpedition" width={800} height={800} quality={100} />
+          </div>
+          <div className="nav__items">
+              <Link href={"/"}>Home</Link>
+              <Link href={"/"}>Ethiopia speciality</Link>
+              <Link href={"#"} className="nav__dropdown">
+                <div>Destinations {"▼"}</div>
+                <div className="nav__dropdown_c">
+                    <Link href={"/"} >The Northern Circuit</Link>
+                    <Link href={"/"} >The Southern Circuit</Link>
+                    <Link href={"/"} >The Eastern Circuit</Link>
+                    <Link href={"/"} >The Western Circuit</Link>
+                </div>
+              </Link>
+              <Link href={"#"} className="nav__dropdown">
+                <div>Tours by experiences {"▼"}</div>
+                <div className="nav__dropdown_c">
+                    <Link href={"/"} >Historical Tours</Link>
+                    <Link href={"/"} >Cultural Tours</Link>
+                    <Link href={"/"} >Photographic Tours</Link>
+                    <Link href={"/"} >Trekking and Hiking Tours</Link>
+                    <Link href={"/"} >Wildlife Safaris</Link>
+                    <Link href={"/"} >Food and Drink Tours</Link>
+                </div>
+              </Link>
+              <Link href={"/"}>Popular ethiopia tours</Link>
+              <Link href={"/"}>Our service</Link>
+              <Link href={"/"}>About us</Link>
+          </div>
+
+          <div className="nav__mobile" onClick={() => toggleMobile()}>
+              <div className="nav__menuContainer">
+                  <HiOutlineMenu />
+              </div>
+          </div>
+      </div>
+
+      <div ref={mobileRef} className="navItems__container__mobile">
+          <div className="mobile__close" onClick={() => toggleMobile()}>
+              <AiOutlineClose />
+          </div>
+          <div className="navItems__mobile">
+              <Link href={"/"} className="navItems__item" onClick={() => toggleMobile()}>Home</Link>
+              <Link href={"/"} className="navItems__item" onClick={() => toggleMobile()}>Ethiopia speciality</Link>
+              <Link href={"/"} className="navItems__item nav__dropdown">
+                  <div>Destinations {"▼"}</div>
+                  <div style={{backgroundColor: "#252324"}} className="nav__dropdown_c">
+                      <Link href={"/"} onClick={() => toggleMobile()} >The Northern Circuit</Link>
+                      <Link href={"/"} onClick={() => toggleMobile()} >The Southern Circuit</Link>
+                      <Link href={"/"} onClick={() => toggleMobile()} >The Eastern Circuit</Link>
+                      <Link href={"/"} onClick={() => toggleMobile()} >The Western Circuit</Link>
+                  </div>
+              </Link>
+              <Link href={"/"} className="navItems__item nav__dropdown">
+                  <div>Tours by experiences {"▼"}</div>
+                  <div style={{backgroundColor: "#252324"}} className="nav__dropdown_c">
+                  <Link href={"/"} >Historical Tours</Link>
+                    <Link href={"/"} onClick={() => toggleMobile()} >Cultural Tours</Link>
+                    <Link href={"/"} onClick={() => toggleMobile()} >Photographic Tours</Link>
+                    <Link href={"/"} onClick={() => toggleMobile()} >Trekking and Hiking Tours</Link>
+                    <Link href={"/"} onClick={() => toggleMobile()} >Wildlife Safaris</Link>
+                    <Link href={"/"} onClick={() => toggleMobile()} >Food and Drink Tours</Link>
+                  </div>
+              </Link>
+              <Link href={"/"} className="navItems__item" onClick={() => toggleMobile()}>Popular ethiopia tours</Link>
+              <Link href={"/"} className="navItems__item" onClick={() => toggleMobile()}>Our service</Link>
+              <Link href={"/"} className="navItems__item" onClick={() => toggleMobile()}>About us</Link>
+          </div>
+      </div>
+    </>
+  )
+}
